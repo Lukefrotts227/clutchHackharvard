ade2699d509e987628d7c7b415e96755a6196f6f
Ljf227 <ljfrotton@gmail.com>
2023-10-22 08:44:11 -0400

    final try

--- client/the-one/app/further/page.js
+++ client/the-one/app/further/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebbb2e321cf6e21d631307cfd00335da6a249cadb8f8366be3ccf00be94323db
-size 724
+oid sha256:548c291ce9671d81a0fa98c18183d2cf0ad23db632091ffb7a2071903e16e73a
+size 522
--- server/javascript/index.js
+++ server/javascript/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a005fdc1a09a97b8c951c1672177cc0353663d915e94452b39d34acfa714ffee
-size 1295
+oid sha256:4a1b18ac00c8326212fa1a32ad74ee066ed44e3dcf44b9f8b91e2684e9ec86e8
+size 1282
